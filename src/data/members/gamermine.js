6ef800919345c6011ca85e45a150d29309803ffd
GamerMine <maxime-sav@outlook.fr>
2023-12-31 00:55:53 +0100

    GamerMine profile completion

--- src/data/members/gamermine.js
+++ src/data/members/gamermine.js
@@ -9,8 +9,13 @@ export default function GamerMine() {
         "Maxime",
         "Savary",
         "Student at IUT of Le Havre",
-        "I did not write my description. I am a bad boy.",
-        [],
+        "Currently in work-study at Capgemini, I worked and still works on various projects. I am open to " +
+        "every type of projects and still wants to improve my experiences and skills.",
+        [
+            "https://github.com/GamerMine",
+            "https://www.youtube.com/channel/UCjtjzedWxrnQwRkdSRVccUg",
+            "https://www.twitch.tv/gamermine55"
+        ],
         <div>
             <div className="section">
                 <h1>Welcome to my page</h1>
