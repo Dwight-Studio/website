195412ce6456dd5ca190b25e8072589d95c2c117
Alexandre Leconte <aleconte@dwightstudio.fr>
2024-01-08 15:24:56 +0100

    Mise à jour de compatibilité Firefox

--- src/data/members/deleranax.js
+++ src/data/members/deleranax.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Member} from "../member";
+import {FaJava} from "react-icons/fa";
 
 export default function Deleranax() {
     return new Member(
@@ -9,15 +10,24 @@ export default function Deleranax() {
         "Alexandre",
         "Leconte",
         "Student at INSA of Rennes",
-        "Currently a student at the INSA Engineering School of Rennes, specializing in cloud computing. I " +
-        "was introduced to programming through the development of mods and plugins for Minecraft back in 2019. Since then, " +
-        "I've maintained a certain affection for Java, its power and portability.",
+        "Portal 2 fanatic, currently a student at the INSA Engineering School of Rennes, specializing" +
+        " in cloud computing. My interests range from game modding to simulation software development. I've got a" +
+        "soft spot for Java.",
         ["https://github.com/Deleranax", "https://www.youtube.com/@deleranax", "https://twitter.com/Deleranax"],
         <div>
             <div className="section">
-                <h1>Welcome to my page</h1>
+                <h1>Led by passion</h1>
                 <p>
-                    I must fill this page, which is empty for now.
+                    For me, coding is a hobby, a pastime that I practice with particular dedication. My projects cover a
+                    wide range of subjects and technologies, as long as they're interesting and rewarding for me.
+                </p>
+            </div>
+            <div className="section">
+                <h1>Have you ever heard of Java? <FaJava/></h1>
+                <p>
+                    I was introduced to programming through the development of mods and plugins for Minecraft back in
+                    2018. Since then, I've maintained a certain affection for Java, its power and portability. Some of
+                    my projects are still on GitHub, but most of them are unmaintained (and not publicly available).
                 </p>
             </div>
         </div>
